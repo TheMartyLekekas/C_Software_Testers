2311b0ed19d381a615d6a5b7ae2eb3d7617508df
TheMartyLekekas <martyhacka@gmail.com>
2020-01-29 13:20:04 +0100

    Create exerc_2_4.c

---
+++ exerc_2_4.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define MAX 10
+#define MAXNUMBER 5
+
+void create_random(int *tab) {    // pointer fills the table
+  *tab = (rand() % (MAXNUMBER + 1));
+}
+
+void count_frequency(int *tab, int *freq) {       // pointer fills in the frequency table
+  int temp = *freq + 1;
+  *freq = temp;
+}
+
+void draw_histogram(int *freq) {    //  histogram is drawn using pointer
+        int jj;
+        for(jj = 0; jj < *freq; jj++) {
+          printf("x");
+        }
+        printf("\n");
+}
+
+int main (void) {
+  int table[MAX], n;
+  int frequency[MAXNUMBER +1];
+  int i;
+
+  for (i = 0; i < MAX; i++) {
+    frequency[i] = 0;
+  }
+
+  for (i = 0; i < MAX; i++) {                       // creates the table and calculates frequency
+    create_random(&table[i]);
+    count_frequency(&table[i], &frequency[table[i]]);
+  }
+
+  for (i = 0; i < MAX; i++) {                       // prints out the table
+    printf("Element[%d] = %d\n", i, table[i]);
+  }
+  printf("\n\n");
+
+  for (i = 0; i <= MAXNUMBER; i++) {                // prints histogram
+    if(frequency[i] > 0) {
+      printf("%d ", i);
+      draw_histogram(&frequency[i]);
+    }
+  }
+
+  return 0;
+}
