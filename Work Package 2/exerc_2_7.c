dff8de18ee19d8311a43531f4e0376f7c789f07a
TheMartyLekekas <martyhacka@gmail.com>
2020-02-04 17:40:01 +0100

    Add control digit control to 2.7

--- Work Package 2/exerc_2_7.c
+++ Work Package 2/exerc_2_7.c
@@ -1,6 +1,6 @@
 /* ====================================
 File name: exerc_2_7.c
-Date: 2020-02-02
+Date: 2020-02-04
 Group nr 11
 Members that contributed to the solutions
 Martynas Lekeckas
@@ -16,17 +16,15 @@ Demonstration code: [<Ass code 1-4> <abc>]Important , No code no exercise points
 #include <string.h>
 
 void readPersnr(char *person);
-int controlDigit(const char *personNum);
+int controlDigit(const char *persnr);
 
 
 int main(void)  {
   char socialNum[11];
   char *person = socialNum;
 
-    do
-    {
-        readPersnr(person);
-
+    do {
+      readPersnr(person);
     } while ((*person != 'q'));
 
     return 0;
@@ -38,11 +36,15 @@ void readPersnr(char *person)
   char month[3];
   char day[3];
   int convert[10];
-  printf("\n\nEnter 10 digits Personal # or q to cancel: \n\n");
+  printf("Enter 10 digits Personal # or q to cancel: \n\n");
   scanf("%s", person);
+  const char *pointer = person;
 
   if (person[0] == 'q') {
-      printf("Program Canceled... Good bye!\n" );
+    printf("Program Cancelled... Good bye!\n" );
+
+  } else if (strlen(person) != 10) {
+    printf("Invalid input\n");
 
   } else {
 
@@ -67,19 +69,43 @@ void readPersnr(char *person)
     person++;
     convert[2] = atoi(day);
 
-    if (convert[0]>00 && convert[0]<=99 && convert[1]>=01 && convert[1]<=12 && convert[2]>=01 && convert[2]<=31) {
-      printf("Personal number is correct\n");
+    if (convert[0]>=00 && convert[0]<=99 && convert[1]>=01 && convert[1]<=12 && convert[2]>=01 && convert[2]<=31) {
+      int scanNum = controlDigit(pointer);
     } else {
-      printf("Incorrect Personal Number\n");
+      printf("Invalid birth date\n\n\n");
     }
-
-    const char *pointer = person;
-    int scanNum = controlDigit(pointer);
-
   }
-
 }
 
-int controlDigit(const char *personNum) {
-  /* code */
+int controlDigit(const char *persnr) {
+  int digits[11], i;
+  char nr[11];
+  for (i = 0; i < 10; i++) {      // convert characters into integers
+    nr[i] = *persnr;
+    persnr++;
+    digits[i] = nr[i] - '0';
+  }
+
+  int sum = 0;
+  for (i = 0; i <= 8; i+=2) {
+    digits[i] *= 2;
+  }
+
+
+  for (i = 0; i <= 8; i++) {
+    if(digits[i] < 10) {
+      sum += digits[i];
+
+    } else{
+      sum += 1 + digits[i] % 10;      // split >=10 integers into digits
+    }
+  }
+
+  sum = 10 - sum % 10;
+  if(sum == 10) sum = 0;
+  printf("Calculated control digit is: %d\n", sum);
+  if(sum == digits[9])
+    printf("Person number is CORRECT\n\n\n");
+  else
+    printf("Given control digit is INCORRECT\n\n\n");
 }
