f3242d173e41a793c1c10d9f341b704f1cc482d0
TheMartyLekekas <martyhacka@gmail.com>
2020-02-25 11:51:09 +0100

    Update exerc_4_4.c

--- Work Package 4/exerc_4_4.c
+++ Work Package 4/exerc_4_4.c
@@ -1,3 +1,16 @@
+/* ====================================
+File name: exerc_4_5.c
+Date: 2020-02-25
+Group nr 11
+Members that contributed to the solutions
+Martynas Lekeckas
+Al-Amir Teniola Abidemi Adegbuji-Onikoyi
+Sebastian Baszczynski
+Member not present at demonstration time:
+-
+Demonstration code: [46713]
+!====================================== */
+
 #include <avr/io.h>
 #include <util/delay.h>
 /* Macros predefined for the compiler
@@ -8,27 +21,24 @@ DDRD  Data direction register D
 PORTD Outport D
 PIND  Inport D*/
 
+
 unsigned char input;
 
 void setup() {
-  Serial.begin(9600);
-	DDRD = 0xFF; // To set all Port D bits as outbits
-	DDRB = 0x00; //Set all to inbits. You will only use bit 0
+  	DDRD = 0xFF; // To set all Port D bits as outbits
+	  DDRB = 0x00; //Set all to inbits. You will only use bit 0
   	PORTD = B00000011;
-  	PINB = B00000001;
-  	PORTB = B00000001;
-  	PINB = PB0;
 }
 
 void loop() {
-  Serial.println(PORTD, BIN);
   if(PINB != B00000001) {
-
     delay(1000);
     PORTD = PORTD << 1;
-
-    if(PORTD == B11000000){
+    if(PORTD == B00000010){
       PORTD = B00000011;
+    }
+    if(PORTD == B10000000){
+      PORTD = B10000001;
     }
   }
 }
