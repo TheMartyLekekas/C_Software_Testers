e69e0b6720b9e3241f61bd0c4be7eca44a1d0a1f
Amir <onikoyiamir@yahoo.com>
2020-03-04 20:50:54 +0100

    exercise 5.1 completed

---
+++ Work Package 5/exerc_5_1.c
@@ -0,0 +1,94 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <Keypad.h>
+#include <Wire.h>
+
+bool keyPressed = false;
+int col;
+char key;
+int row;
+
+
+
+char keys[4][4] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'F','0','E','D'}
+};
+
+void setRowLow(int R) {
+  switch(R){
+    case 3:
+    	PORTB = B00001110;
+          break;
+    case 2:
+    	PORTB = B00001101;
+          break;
+    case 1:
+    	PORTB = B00001011;
+          break;
+    case 0:
+    	PORTB = B00000111;
+          break;
+  }
+}
+
+int checkColumn(){
+  if((PIND & (1 << PD7)) == 0){
+    return 0;
+  }
+  else if((PIND & (1 << PD6)) == 0){
+    return 1;
+  }
+  else if((PIND & (1 << PD5)) == 0){
+    return 2;
+  }
+  else if((PIND & (1 << PD4)) == 0){
+    return 3;
+  }
+  else{
+    return -1;
+  }
+}
+
+
+void keyboardirq(){
+  //iterate over the rows
+    for(int i=0; i < 4; i++) {
+      row = i;
+      setRowLow(i);
+      col = checkColumn();
+
+      if(col != -1){
+        keyPressed = true;
+        key = keys[row][col];
+        break;
+      }
+
+    }
+
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  DDRD = 0x00;
+  DDRB = 0b00001111;
+
+  attachInterrupt(digitalPinToInterrupt(2), keyboardirq , FALLING);
+
+}
+
+void loop()
+{
+    col = 0;
+    row = 0;
+    PORTB = 0b00000000;
+  if (keyPressed){
+    Serial.println(key);
+  }
+  keyPressed = false;
+
+
+}
